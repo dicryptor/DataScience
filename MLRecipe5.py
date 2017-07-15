5b0817ec0ca1bb4aee68459b85a3008f73a07bfb
wonderlander <mohamedmubaraq.abdulhamid@gmail.com>
2017-07-15 13:30:04 +0800

    updated MLRecipe5 and added MLRecipe6

--- MLRecipe5.py
+++ MLRecipe5.py
@@ -3,8 +3,36 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+import random
+from scipy.spatial import distance
+
+def euc(a, b):
+    return distance.euclidean(a, b)
+
 
 iris = datasets.load_iris()
+
+class ScrappyKNN():
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+
+    def predict(self, X_test):
+        predictions = []
+        for row in X_test:
+            label = self.closest(row)
+            predictions.append(label)
+        return predictions
+
+    def closest(self, row):
+        best_dist = euc(row, self.X_train[0])
+        best_index = 0
+        for i in range(1, len(self.X_train)):
+            dist = euc(row, self.X_train[i])
+            if dist < best_dist:
+                best_dist = dist
+                best_index = i
+        return self.y_train[best_index]
 
 X = iris.data
 Y = iris.target
@@ -22,3 +50,9 @@ my_classifier2.fit(X_train, y_train)
 predictions2 = my_classifier2.predict(X_test)
 print(predictions2)
 print(accuracy_score(y_test, predictions2))
+
+my_classifier3 = ScrappyKNN()
+my_classifier3.fit(X_train, y_train)
+predictions3 = my_classifier3.predict(X_test)
+print(predictions3)
+print(accuracy_score(y_test, predictions3))
